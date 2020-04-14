fbefd7b6d738f5068658b3cd4c2a4cf63259265e
Treek <nbatre1@yahoo.com>
2020-04-14 16:31:27 -0500

    keep moving forward

--- sudoko/src/App.js
+++ sudoko/src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import "bulma/css/bulma.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div classNameName="App">
+      
+      <button className="button is-white">White</button>
+<button className="button is-light">Light</button>
+<button className="button is-dark">Dark</button>
+<button className="button is-black">Black</button>
+<button className="button is-text">Text</button>
+
     </div>
   );
 }
